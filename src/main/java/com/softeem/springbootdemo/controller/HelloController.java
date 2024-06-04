49e0eea9c3b94a2682ac7b03cfd60565a0423b42
mincai <2310956312@qq.com>
2024-06-04 15:40:04 +0800

    mincai

--- src/main/java/com/softeem/springbootdemo/controller/HelloController.java
+++ src/main/java/com/softeem/springbootdemo/controller/HelloController.java
@@ -8,44 +8,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
     //json:没  ajax:没  vue.js:没  jsp网页:学  thymeleaf:没
     @RequestMapping("/main")
-    public String main(){
+    public String main() {
         return "main";
     }
 
     @RequestMapping("/top")
-    public String top(){
+    public String top() {
         return "top";
     }
 
     @RequestMapping("/left")
-    public String left(){
+    public String left() {
         return "left";
     }
 
     @RequestMapping("/show")
-    public String show(){
+    public String show() {
         return "show";
     }
+
     @RequestMapping("/addBook")
-    public String addBook(){
+    public String addBook() {
         return "addbook";
     }
 
     /**
      * 新添加的方法哈哈
+     *
      * @return 返回裴杰
      * @author peige
      */
     @RequestMapping("/addRecord")
     @ResponseBody
-    public String addRecord(){
+    public String addRecord() {
         System.out.println("裴杰");
         return "peijie";
     }
 
     @RequestMapping("/addRecord2")
     @ResponseBody
-    public String addRecord2(){
+    public String addRecord2() {
         System.out.println("裴杰2");
         return "peijie";
     }
